959f49626246093f5b6f8ab4d7344d5cbb7ae49b
Scott <scott.chaplinski@gmail.com>
2019-09-07 13:24:25 -0500

    added logging statement for onCreate

--- app/src/main/java/com/example/scott/helloworld/MainActivity.java
+++ app/src/main/java/com/example/scott/helloworld/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.scott.helloworld;
 
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Type in "logd<TAB>" to have this added automatically
+        Log.d("MainActivity", "onCreate: This is in onCreate!");
     }
+
+
+
+
 }

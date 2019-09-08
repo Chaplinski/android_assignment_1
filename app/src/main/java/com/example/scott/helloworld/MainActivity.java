1b5625ae483dacf4de6590a4e220dad0bd313405
Scott <scott.chaplinski@gmail.com>
2019-09-08 18:47:50 -0500

    bundle working

--- app/src/main/java/com/example/scott/helloworld/MainActivity.java
+++ app/src/main/java/com/example/scott/helloworld/MainActivity.java
@@ -18,31 +18,35 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textDegreesTop;
+    private TextView inputText;
     private TextView textDegreesBottom;
+    private TextView textResult;
     private TextView textConversionHistory;
-    private TextView textInputBottom;
     private int radioValue = 0;
     ArrayList<String> conversionHistory = new ArrayList<String>();
+    private String TAG = "BUNDLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textDegreesTop = (TextView) findViewById(R.id.textDegreesTop);
+        inputText = (EditText)findViewById(R.id.textInputTop);
+        textDegreesBottom = (TextView) findViewById(R.id.textDegreesBottom);
+        textResult = (TextView) findViewById(R.id.textResult);
+        textConversionHistory = (TextView) findViewById(R.id.textInputHistory);
     }
 
     //radio methods
     public void radioFahrenheitClicked(View v) {
-        textDegreesTop = (TextView) findViewById(R.id.textDegreesTop);
         textDegreesTop.setText("Fahrenheit Degrees: ");
-        textDegreesBottom = (TextView) findViewById(R.id.textDegreesBottom);
         textDegreesBottom.setText("Celsius Degrees: ");
         radioValue = 0;
     }
 
     public void radioCelsiusClicked(View v) {
-        textDegreesTop = (TextView) findViewById(R.id.textDegreesTop);
         textDegreesTop.setText("Celsius Degrees: ");
-        textDegreesBottom = (TextView) findViewById(R.id.textDegreesBottom);
         textDegreesBottom.setText("Fahrenheit Degrees: ");
         radioValue = 1;
     }
@@ -51,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
     //method for when convert button is pressed
     public void convertButtonClicked(View v) {
-        EditText inputText = (EditText)findViewById(R.id.textInputTop);
         //this if statement prevents the app from crashing if there is no value input
         if (inputText.length() > 0) {
             String stringResult = inputText.getText().toString();
@@ -74,8 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 stringFinalResult = floatResult + " C ==> " + formattedValue + " F";
             }
 
-            textInputBottom = (TextView) findViewById(R.id.textResult);
-            textInputBottom.setText(formattedValue);
+            textResult.setText(formattedValue);
 
             //add string value to conversion history string array
             conversionHistory.add(0, stringFinalResult);
@@ -87,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
             //set conversion history
-            textConversionHistory = (TextView) findViewById(R.id.textInputHistory);
             textConversionHistory.setText(stringConversionHistory);
 
 
@@ -98,13 +99,38 @@ public class MainActivity extends AppCompatActivity {
     public void clearButtonClicked(View v) {
         //clear conversionHistory array and set conversion history text to empty string
         conversionHistory.clear();
-        textConversionHistory = (TextView) findViewById(R.id.textInputHistory);
         textConversionHistory.setText("");
-
     }
 
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+//        Log.d(TAG, "onSaveInstanceState: " + textDegreesTop.getText().toString());
+        outState.putString("TEXTDEGREESTOP", textDegreesTop.getText().toString());
+        outState.putString("TEXTDEGREESBOTTOM", textDegreesBottom.getText().toString());
+        outState.putString("TEXTRESULT", textResult.getText().toString());
+        outState.putString("TEXTHISTORY", textConversionHistory.getText().toString());
+        outState.putStringArrayList("ARRAYHISTORY", conversionHistory);
+        outState.putInt("RADIOVALUE", radioValue);
 
+        // Call super last
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Call super first
+        super.onRestoreInstanceState(savedInstanceState);
+
+        textDegreesTop.setText(savedInstanceState.getString("TEXTDEGREESTOP"));
+        textDegreesBottom.setText(savedInstanceState.getString("TEXTDEGREESBOTTOM"));
+        textResult.setText(savedInstanceState.getString("TEXTRESULT"));
+        textConversionHistory.setText(savedInstanceState.getString("TEXTHISTORY"));
+
+        conversionHistory = savedInstanceState.getStringArrayList("ARRAYHISTORY");
+
+        radioValue = savedInstanceState.getInt("RADIOVALUE");
+    }
 
 
 
